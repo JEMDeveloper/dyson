ef935c3fdde15ccb6b37efa834c09a233645dba2
JEMDeveloper <jemdeveloper@develop.com>
2018-10-10 23:21:30 -0400

    Updated app center for dev, qa and prod environments + progress bar

--- src/iOS/Dyson.swift
+++ src/iOS/Dyson.swift
@@ -2,26 +2,26 @@ import Dyson
 
 @available(iOS 10.0, *)
 @objc(Dyson) class Dyson : CDVPlugin {
-    
+
     private var hasSubscribedToNotifications = false
     var completedUploadsIds : Set<String>!
-    
+
     @objc(setEnvironment:)
     func setEnvironment(command: CDVInvokedUrlCommand) {
-        
+
         self.completedUploadsIds = Set<String>()
-        
+
         //        UploadManager.sharedInstance.emptyDataBase()
-        
+
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
-        
+
         if let environment = command.arguments[0] as? String
         {
             UploadManager.sharedInstance.environment = environment
             Logger.sharedInstance.isLoggingEnabled = true
-            
+
             pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_OK,
                 messageAs: "Environment Updated Successfully"
@@ -32,53 +32,58 @@ import Dyson
                 messageAs: "Environment Update Failed"
             )
         }
-        
+
         if !hasSubscribedToNotifications
         {
             NotificationHelper.sharedInstance.subscribeToPendingUploadsEvent(observer: self, selector: #selector(updateUploadsLeft(_:)))
             NotificationHelper.sharedInstance.subscribeToUploadCompletedEvent(observer: self, selector: #selector(uploadCompleted(_:)))
             NotificationHelper.sharedInstance.subscribeToChangeStatusEvent(observer: self, selector: #selector(changeStatus(_:)))
             NotificationHelper.sharedInstance.subscribeToReachabilityChanged(observer: self, selector: #selector(changeWifiIconColor(_:)))
-            
+
             self.hasSubscribedToNotifications = true
         }
-        
+
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
         )
-        
+
     }
-    
+
     @objc(getPendingUploads:)
     func getPendingUploads(command: CDVInvokedUrlCommand) {
-        
+
         let pendingUploads = UploadManager.sharedInstance.getTotalPendingUploads()
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: pendingUploads)
-        
+
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
         )
-        
+
     }
-    
+
     @objc(getEnrollmentProgress:)
     func getEnrollmentProgress(command: CDVInvokedUrlCommand) {
-        
-        let pendingUploads = UploadManager.sharedInstance.getPercentageCompleted()
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: pendingUploads)
-        
+
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+        let isconnectionAvailable = NetworkManager.sharedInstance.isConnectedToNetwork()
+        if (isconnectionAvailable){
+            let pendingUploads = UploadManager.sharedInstance.getTotalPendingUploads()
+            let filesUploaded = UploadManager.sharedInstance.getTotalFilesUploaded()
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: [pendingUploads,filesUploaded])
+        }
+
         self.commandDelegate!.send(
             pluginResult,
             callbackId: command.callbackId
         )
-        
+
     }
-    
+
     @objc(getCompleteUploadIds:)
     func getCompleteUploadIds(command: CDVInvokedUrlCommand) {
-        
+
         var result = ""
         if let arrayOfIdsToCheck = command.arguments as? [String]
         {
@@ -91,7 +96,7 @@ import Dyson
             callbackId: command.callbackId
         )
     }
-    
+
     @objc(updateIcNumber:)
     func updateIcNumber(command: CDVInvokedUrlCommand) {
         if let icNumber = command.arguments[0] as? String
@@ -100,31 +105,31 @@ import Dyson
             UploadManager.sharedInstance.update(ICNumber: icNumber)
         }
     }
-    
+
     @objc(startTransmissionOfUploadsOnLogin:)
     func startTransmissionOfUploadsOnLogin(command: CDVInvokedUrlCommand) {
         DispatchQueue.global(qos: .background).async {
             UploadManager.sharedInstance.startUploadingEnrollmentsFromDB(andKeychain: true)
         }
     }
-    
+
     @objc(startTransmissionOfUploads:)
     func startTransmissionOfUploads(command: CDVInvokedUrlCommand) {
         DispatchQueue.global(qos: .background).async {
             UploadManager.sharedInstance.startUploadingEnrollmentsFromDB(andKeychain: false)
         }
     }
-    
+
     @objc(removeSuccessfulRecordsFromDevice:)
     func removeSuccessfulRecordsFromDevice(command: CDVInvokedUrlCommand) {
         DispatchQueue.global(qos: .background).async {
             UploadManager.sharedInstance.removeSuccessfulRecordsFromDevice()
         }
     }
-    
+
     @objc(startTransmissionOfEnrollmentEntry:)
     func startTransmissionOfEnrollmentEntry(command: CDVInvokedUrlCommand) {
-        
+
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
@@ -133,12 +138,12 @@ import Dyson
             let enrollmentIdentifier = command.arguments[2] as? String
         {
             DispatchQueue.global(qos: .background).async {
-                
+
                 let enrollmentEntry = ACPTModel(uniqueMessageID: uniqueId, data: enrollmentData, status: "\(EnrollmentStatusType.SEND_TO_HQ_INITIATED)", enrollmentIdentifier: enrollmentIdentifier, rCount: 0)
-                
+
                 UploadManager.sharedInstance.startTransmissionOfCompleteEnrollment(entry: enrollmentEntry)
                 { (error, response) in
-                    
+
                     if(error == nil){
                         pluginResult = CDVPluginResult(
                             status: CDVCommandStatus_OK,
@@ -154,38 +159,38 @@ import Dyson
                         )
                         Logger.sharedInstance.logError(error: "Data Send Failed! : \(error?.localizedDescription)")
                     }
-                    
+
                     self.commandDelegate!.send(
                         pluginResult,
                         callbackId: command.callbackId
                     )
-                    
+
                 }
-                
+
             }
-            
+
         }else{
             pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_ERROR,
                 messageAs: "Invalid parameters"
             )
-            
+
             self.commandDelegate!.send(
                 pluginResult,
                 callbackId: command.callbackId
             )
-            
+
         }
-        
+
     }
-    
+
     @objc(startTransmissionOfBlob:)
     func startTransmissionOfBlob(command: CDVInvokedUrlCommand) {
-        
+
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
         )
-        
+
         if let enrollmentData = command.arguments[0] as? String,
             let uniqueId = command.arguments[1] as? String,
             let blobFolder = command.arguments[2] as? String,
@@ -193,11 +198,11 @@ import Dyson
         {
             DispatchQueue.global(qos: .background).async {
                 let enrollmentEntry = ACPTModel(uniqueMessageID: uniqueId, data: enrollmentData, status: "\(EnrollmentStatusType.SEND_TO_BLOB_INITIATED)", enrollmentIdentifier: enrollmentIdentifier, rCount: 0)
-                
+
                 UploadManager.sharedInstance.startTransmissionOfBlob(entry: enrollmentEntry, blobFolder: blobFolder)
                 {
                     (error, response) in
-                    
+
                     if(error == nil){
                         pluginResult = CDVPluginResult(
                             status: CDVCommandStatus_OK,
@@ -213,37 +218,37 @@ import Dyson
                         )
                         Logger.sharedInstance.logError(error: "Blob Send Failed! : \(String(describing: error?.localizedDescription))")
                     }
-                    
+
                     self.commandDelegate!.send(
                         pluginResult,
                         callbackId: command.callbackId
                     )
-                    
+
                 }
             }
-            
+
         }else{
             pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_ERROR,
                 messageAs: "Invalid parameters"
             )
-            
+
             self.commandDelegate!.send(
                 pluginResult,
                 callbackId: command.callbackId
             )
-            
+
         }
-        
+
     }
-    
+
     @objc private func updateUploadsLeft(_ notification : NSNotification){
         let pendingUploads = notification.userInfo?["pendingUploads"] as! Int
         let command = CDVPluginHelper.sharedInstance.getJavascriptChangeUpdateCurrentUploadedItemsCommandFor(uploadsLeft: pendingUploads)
         Logger.sharedInstance.logInfo(info: "updateUploadsLeft : \(command)")
         self.commandDelegate.evalJs(command)
     }
-    
+
     @objc private func uploadCompleted(_ notification : NSNotification){
         let messageId = notification.userInfo?["messageId"] as! String
         //        Logger.sharedInstance.logInfo(info: "uploadCompleted : \(self.completedUploadsIds)")
@@ -251,20 +256,20 @@ import Dyson
         let command = "window.uploadCompleted();"
         self.commandDelegate.evalJs(command)
     }
-    
+
     @objc private func changeStatus(_ notification : NSNotification){
         let status = notification.userInfo?["status"] as! Int
         let command = CDVPluginHelper.sharedInstance.getJavascriptChangeStatusCommandFor(status : status)
         Logger.sharedInstance.logInfo(info: "changeStatus : \(command)")
         self.commandDelegate.evalJs(command)
     }
-    
+
     @objc private func changeWifiIconColor(_ notification : NSNotification){
         let colorStatus = notification.userInfo?["ColorStatus"] as! String
         let command = CDVPluginHelper.sharedInstance.getUpdateWifiIconCommandFor(NetworkStatusColor : colorStatus)
         Logger.sharedInstance.logInfo(info: "changeColorStatus : \(command)")
         self.commandDelegate.evalJs(command)
     }
-    
+
 }
 
